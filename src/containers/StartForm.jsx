96b91444e1fa68a0d976de5c5bf3f48533443e6b
kwangyoulsagong <sgky0511@naver.com>
2024-07-26 13:41:26 +0900

    chore: prettier adapted

--- src/containers/StartForm.jsx
+++ src/containers/StartForm.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import styles from "./styles/StartForm.module.scss"
+import styles from './styles/StartForm.module.scss';
 import Button from '../components/Button';
-import {useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 function StartForm() {
-    const navigate=useNavigate()
-    // 로그인 페이지 이동
-    const handleLoginPage=()=>{
-        navigate("/login")
-    }
+  const navigate = useNavigate();
+  // 로그인 페이지 이동
+  const handleLoginPage = () => {
+    navigate('/login');
+  };
 
-    // 회원가입 페이지 이동
-    const handleSignupPage=()=>{
-        navigate("/signup")
-    }
+  // 회원가입 페이지 이동
+  const handleSignupPage = () => {
+    navigate('/signup');
+  };
   return (
     <main className={styles.main}>
       <header className={styles.logoContainer}>
-          <div className={styles.logo}></div>
+        <div className={styles.logo}></div>
       </header>
       <section className={styles.buttons}>
-          <article onClick={handleLoginPage}>
-              <Button variant={"login"}>로그인하기</Button>
-          </article>
-          <article onClick={handleSignupPage}>
-              <Button variant={"signup"}>회원가입하기</Button>
-          </article>
+        <article onClick={handleLoginPage}>
+          <Button variant={'login'}>로그인하기</Button>
+        </article>
+        <article onClick={handleSignupPage}>
+          <Button variant={'signup'}>회원가입하기</Button>
+        </article>
       </section>
     </main>
   );
--- src/hooks/useFormHandler.jsx
+++ src/hooks/useFormHandler.jsx
@@ -3,34 +3,37 @@ import { ActionType } from '../reducers/Register';
 import useFormValidation from './useValidation';
 
 const useFormHandlers = (state, dispatch) => {
-    const { validateField, errors } = useFormValidation(state);
+  const { validateField, errors } = useFormValidation(state);
 
-    const handleInputChange = useCallback((type, value) => {
-        dispatch({ type, payload: value });
+  const handleInputChange = useCallback(
+    (type, value) => {
+      dispatch({ type, payload: value });
 
-        // 인풋 값이 change 될때 유효성 검사
-        switch (type) {
-            case ActionType.SET_ID:
-                validateField('id', value);
-                break;
-            case ActionType.SET_NICKNAME:
-                validateField('nickname', value);
-                break;
-            case ActionType.SET_PASSWORD:
-                validateField('password', value);
-                break;
-            case ActionType.SET_VERIFYPASSWORD:
-                validateField('verifyPassword', value);
-                break;
-            case ActionType.SET_WEIGHT:
-                validateField('weight', value);
-                break;
-            default:
-                break;
-        }
-    }, [dispatch, validateField]);
+      // 인풋 값이 change 될때 유효성 검사
+      switch (type) {
+        case ActionType.SET_ID:
+          validateField('id', value);
+          break;
+        case ActionType.SET_NICKNAME:
+          validateField('nickname', value);
+          break;
+        case ActionType.SET_PASSWORD:
+          validateField('password', value);
+          break;
+        case ActionType.SET_VERIFYPASSWORD:
+          validateField('verifyPassword', value);
+          break;
+        case ActionType.SET_WEIGHT:
+          validateField('weight', value);
+          break;
+        default:
+          break;
+      }
+    },
+    [dispatch, validateField]
+  );
 
-    return { handleInputChange, validateField, errors };
+  return { handleInputChange, validateField, errors };
 };
 
 export default useFormHandlers;
