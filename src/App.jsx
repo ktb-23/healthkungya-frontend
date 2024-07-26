46361e441e01a4d27cbb4ca70b972d25408b7813
kwangyoulsagong <sgky0511@naver.com>
2024-07-26 13:35:34 +0900

    chore: prettier adapted

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StartRoutes from './routes/StartRoutes';
 import './App.css';
-import LoginForm from './containers/LoginForm';
 function App() {
   return (
     <Router>
-      <StartRoutes/>
-  </Router>
+      <StartRoutes />
+    </Router>
   );
 }
 
--- src/containers/LoginForm.jsx
+++ src/containers/LoginForm.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import styles from "./styles/LoginForm.module.scss"
+import styles from './styles/LoginForm.module.scss';
 function LoginForm() {
   return (
     <main className={styles.main}>
       <div className={styles.logo}></div>
-      <section className={styles.inputs}>
-      </section>
+      <section className={styles.inputs}></section>
     </main>
   );
 }
