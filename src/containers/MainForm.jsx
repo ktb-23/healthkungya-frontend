6f17b7dc81080ac28c41a03bd82c1ac0b5d646a4
parkseyoung <hellosy76@naver.com>
2024-07-30 17:07:11 +0900

    feat: index (식단, 운동, 체중) 생성

--- src/containers/MainForm.jsx
+++ src/containers/MainForm.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import FixForm from './FixForm.jsx';
+import Index from '../components/Index.jsx';
+import Foodindex from '../picture/foodindex.svg';
+import Exindex from '../picture/exindex.svg';
+import Weightindex from '../picture/weightindex.svg';
 import './styles/MainForm.scss';
 const MainForm = () => {
   return (
     <>
       <FixForm />
+      <div className="index-container">
+        <Index indexicon={Foodindex} output="식단" />
+        <Index indexicon={Exindex} output="운동" />
+        <Index indexicon={Weightindex} output="체중" />
+      </div>
     </>
   );
 };
