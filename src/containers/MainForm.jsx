aee5451772ebac6899fadc510a87a48e30d1ab74
parkseyoung <hellosy76@naver.com>
2024-07-30 16:06:16 +0900

    docs: 필요없는 import 삭제

--- src/containers/MainForm.jsx
+++ src/containers/MainForm.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import FixForm from './FixForm.jsx';
-import Index from '../components/Index.jsx';
-import Foodindex from '../picture/foodindex.svg';
-import Exindex from '../picture/exindex.svg';
-import Weightindex from '../picture/weightindex.svg';
+
 import './styles/MainForm.scss';
 const MainForm = () => {
   return (
