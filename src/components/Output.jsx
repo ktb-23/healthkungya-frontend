588f5b9a357a15fe7ff9146bb486ee9d2e97c1f2
parkseyoung <hellosy76@naver.com>
2024-07-31 17:42:58 +0900

    feat: output 넣기 완료

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,1 +1,30 @@
-//NOTE:식사, 운동 입력받은 값 보여주는 컴포넌트
+import React from 'react';
+import styles from './styles/Output.module.scss'; // CSS 모듈을 임포트합니다.
+
+const Output = ({ text, children }) => {
+  // text에 따라서 동적으로 클래스 네임을 획득
+  const getOutputClass = () => {
+    switch (text) {
+      case '식단-아침':
+        return styles.breakfastoutput;
+      case '식단-점심':
+        return styles.lunchoutput;
+      case '식단-저녁':
+        return styles.dinneroutput;
+      case '운동':
+        return styles.exoutput;
+      case '운동소모':
+        return styles.exkcaloutput;
+      case '체중':
+        return styles.weightoutput;
+      default:
+        return styles.defaultoutput;
+    }
+  };
+
+  return (
+    <div className={`${styles.outputbox} ${getOutputClass()}`}>{children}</div>
+  );
+};
+
+export default Output;
--- src/containers/MainForm.jsx
+++ src/containers/MainForm.jsx
@@ -6,6 +6,8 @@ import Exindex from '../picture/exindex.svg';
 import Weightindex from '../picture/weightindex.svg';
 import './styles/MainForm.scss';
 import Button from '../components/Button.jsx';
+import Output from '../components/Output.jsx';
+
 const MainForm = () => {
   return (
     <>
@@ -19,6 +21,14 @@ const MainForm = () => {
         <Button variant={'foodchange'}>수정하기</Button>
         <Button variant={'exchange'}>수정하기</Button>
         <Button variant={'weightchange'}>수정하기</Button>
+      </div>
+      <div className="mainoutput-container">
+        <Output text="식단-아침">아침</Output>
+        <Output text="식단-점심">점심</Output>
+        <Output text="식단-저녁">저녁</Output>
+        <Output text="운동">운동</Output>
+        <Output text="운동소모">소모칼로리</Output>
+        <Output text="체중">체중</Output>
       </div>
     </>
   );
