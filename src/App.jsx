7c6003b190c7ef42ce3212c5638c189261a9aa5d
kwangyoulsagong <sgky0511@naver.com>
2024-08-08 22:53:03 +0900

    fix: change branch name

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StartRoutes from './routes/StartRoutes';
 import './App.css';
+import PageRoutes from './routes/PageRoutes';
 function App() {
   return (
     <Router>
       <StartRoutes />
+      <PageRoutes />
     </Router>
   );
 }
