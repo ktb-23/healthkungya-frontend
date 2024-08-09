997d1c3b5fd2e90aba2532d02f94297b0e1d7b65
kwangyoulsagong <sgky0511@naver.com>
2024-08-09 17:56:56 +0900

    feat: 운동 검색 기능: useDebounce를 활용하여 0.5초뒤에 서버에게 관련된 검색어 요청 그 후 검색 wrapper에 리스트 나열 #10

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,53 @@
 //NOTE:운동검색 컴포넌트
+import { useEffect, useState } from 'react';
+import Input from './Input';
 import styles from './styles/Search.module.scss';
+import searchIcon from '../picture/search.svg';
+import useDebounce from '../hooks/useDebounce';
+import useSearch from '../api/useSearch';
+
 const Search = () => {
-  return <div className={styles.wrapper}></div>;
+  const [search, setSearch] = useState('');
+  const autoComplete = useDebounce(search, 500);
+  const [data, setData] = useState();
+  const onHandleChange = (e) => {
+    setSearch(e.target.value);
+  };
+  const fetchExitem = async () => {
+    try {
+      const response = await useSearch(search);
+      setData(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchExitem();
+  }, [autoComplete]);
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.searchWrapper}>
+        <Input
+          type="text"
+          value={search}
+          onChange={onHandleChange}
+          placeholder="운동 검색"
+          variant={'searchInput'}
+        />
+        <img src={searchIcon}></img>
+      </div>
+      <div className={styles.items}>
+        {data && data.length > 0 ? (
+          data.map((item) => (
+            <div className={styles.itemWrapper} key={item.id}>
+              {item.ex}
+            </div>
+          ))
+        ) : (
+          <div>검색 결과가 없습니다.</div>
+        )}
+      </div>
+    </div>
+  );
 };
 export default Search;
